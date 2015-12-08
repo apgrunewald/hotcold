f601141747903f94a34978afb3f5bb1a54b64536
apgrunewald <apgrunewald@gmail.com>
2015-12-07 21:09:47 -0800

    --all

---
+++ js/app.js
@@ -0,0 +1,105 @@
+
+$(document).ready(function(){
+
+var numRand = Math.floor((Math.random() * 101) + 1); //Random Number Generator at page load
+
+
+//-----------------------------------------------Guessing Number Begin
+$('#guessButton').click(function() {
+  var guessNum = $("#userGuess").val(); //is number between 1 and 100
+  var counter = $('#count').text(); //counts number of guesses
+  counter++;
+  $('#count').text(counter);
+  var getNum = $('input#userGuess').val(); //user's input
+  
+   if (guessNum > 100 || guessNum < 1) { //check if number is between 1 and 100
+      alert("Please insert a number between 1 and 100!");
+    }
+    else { //run the game 
+ //---------------------------------------------Correct Guess Begin
+  if (getNum == numRand) {
+    $('#feedback').text('Correct!');
+    alert('You Got It! Click "+ New Game" to Play Again!');
+    event.preventDefault();
+  }
+  //---------------------------------------------Correct Guess End
+
+
+  //---------------------------------------------Within 50 Begin
+  else if (Math.abs(numRand - getNum) > 50) {
+    $('#guessList').append('<li>' + $('input#userGuess').val() + '</li>');
+    $('input#userGuess').val(" ");
+    $('#feedback').text('ice cold!');
+  }
+  //---------------------------------------------Within 50 End
+
+
+  //---------------------------------------------Within 40 Begin
+  else if (Math.abs(numRand - getNum) > 40) {
+    $('#guessList').append('<li>' + $('input#userGuess').val() + '</li>');
+    $('#feedback').text('cold!');
+    $('input#userGuess').val(" ");
+  }
+  //---------------------------------------------Within 40 End 
+
+
+  //---------------------------------------------Within 30 Begin
+  else if (Math.abs(numRand - getNum) > 30) {
+    $('#guessList').append('<li>' + $('input#userGuess').val() + '</li>');
+    $('#feedback').text('warm!');
+    $('input#userGuess').val(" ");
+  }
+  //---------------------------------------------Within 30 End
+
+
+  //---------------------------------------------Within 20 Begin
+  else if (Math.abs(numRand - getNum) > 20) {
+    $('#guessList').append('<li>' + $('input#userGuess').val() + '</li>');
+    $('#feedback').text('hot!');
+    $('input#userGuess').val(" ");
+  }
+  //---------------------------------------------Within 20 End
+
+
+  //---------------------------------------------Within 10 Begin
+  else if (Math.abs(numRand - getNum) >= 1) {
+    $('#guessList').append('<li>' + $('input#userGuess').val() + '</li>');
+    $('#feedback').text('very hot!');
+    $('input#userGuess').val(" ");
+  }
+  //---------------------------------------------Within 10 End
+
+    }
+
+});
+//-----------------------------------------------Guessing Number Ends
+
+//-----------------------------------------------New Game Begins
+$('.new').click(function() {
+  $('#guessList').empty();
+  $('input#userGuess').val(" ");
+  $('#feedback').text('Make Your Guess!');
+  $('#count').text("0");
+
+
+  numRand = Math.floor((Math.random() * 101) + 1); //Random number generator at "new game" button click
+  alert(numRand);
+
+});
+//-----------------------------------------------New Game End
+
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+});
+
+
